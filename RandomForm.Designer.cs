6e48b95aac4632774e28e8d4e3b6102aefa49e42
bax082024 <bax082024@gmail.com>
2024-12-27 21:24:22 +0100

    added label for output name

--- RandomForm.Designer.cs
+++ RandomForm.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            listBox1 = new ListBox();
             listBox2 = new ListBox();
+            listBox1 = new ListBox();
+            btnSelectedName = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(btnSelectedName);
             panel1.Controls.Add(listBox2);
             panel1.Controls.Add(listBox1);
             panel1.Dock = DockStyle.Fill;
@@ -45,8 +47,19 @@
             panel1.Size = new Size(1074, 721);
             panel1.TabIndex = 0;
             // 
+            // listBox2
+            // 
+            listBox2.Anchor = AnchorStyles.None;
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(808, 213);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(200, 319);
+            listBox2.TabIndex = 1;
+            // 
             // listBox1
             // 
+            listBox1.Anchor = AnchorStyles.None;
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
             listBox1.Location = new Point(48, 213);
@@ -54,14 +67,15 @@
             listBox1.Size = new Size(200, 319);
             listBox1.TabIndex = 0;
             // 
-            // listBox2
+            // btnSelectedName
             // 
-            listBox2.FormattingEnabled = true;
-            listBox2.ItemHeight = 15;
-            listBox2.Location = new Point(808, 213);
-            listBox2.Name = "listBox2";
-            listBox2.Size = new Size(200, 319);
-            listBox2.TabIndex = 1;
+            btnSelectedName.AutoSize = true;
+            btnSelectedName.Font = new Font("Stencil", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSelectedName.Location = new Point(420, 327);
+            btnSelectedName.Name = "btnSelectedName";
+            btnSelectedName.Size = new Size(115, 32);
+            btnSelectedName.TabIndex = 2;
+            btnSelectedName.Text = "label1";
             // 
             // RandomForm
             // 
@@ -73,6 +87,7 @@
             Name = "RandomForm";
             Text = "Random Name Selector";
             panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -81,5 +96,6 @@
         private Panel panel1;
         private ListBox listBox2;
         private ListBox listBox1;
+        private Label btnSelectedName;
     }
 }
